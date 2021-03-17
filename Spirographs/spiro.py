bd82ecf7a9b22ae78de2f9f5ee65a5fb2b5a9a09
Tim Bossuyt <tim.bossuyt@outlook.com>
2021-03-17 21:11:52 +0100

    Update

--- Spirographs/spiro.py
+++ Spirographs/spiro.py
@@ -1,25 +1,17 @@
 import math
 import turtle
 
-def drawcircle(r):
-    turtle.up()
-    turtle.setpos(r, 0)
-    turtle.down()
 
-    for i in range(0, 365):
-        a = math.radians(i)
-        x = math.cos(a) * r
-        y = math.sin(a) * r
-        turtle.setpos(x, y)
 
 
 def drawspiro(R, r, l):
+    turtle.shape('turtle')
     turtle.up()
-    turtle.setpos(R + r, 0)
+    turtle.setpos(R, 0)
     turtle.down()
     k = r/float(R)
 
-    for i in range(0, 2000):
+    for i in range(0, 10000):
         t = math.radians(i)
 
         x = R*((1 - k)*math.cos(t) + l*k*math.cos((1-k)*t/k))
@@ -28,7 +20,7 @@ def drawspiro(R, r, l):
 
 
 
-drawspiro(100, 30, 5)
+drawspiro(297, 32, 5)
 turtle.mainloop()
 
 
